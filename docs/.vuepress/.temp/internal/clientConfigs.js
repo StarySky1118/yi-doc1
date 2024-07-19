c336042c2044650e5be892021e45da1aa853dc2b
ZhangZiYi <ecustzzy1@163.com>
2024-07-19 22:10:26 +0800

    feat:initial commit

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,19 @@
+import * as clientConfig0 from 'Z:/code3/yi-doc-1/vuepress-starter/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from 'Z:/code3/yi-doc-1/vuepress-starter/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from 'Z:/code3/yi-doc-1/vuepress-starter/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from 'Z:/code3/yi-doc-1/vuepress-starter/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig4 from 'Z:/code3/yi-doc-1/vuepress-starter/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig5 from 'Z:/code3/yi-doc-1/vuepress-starter/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig6 from 'Z:/code3/yi-doc-1/vuepress-starter/node_modules/@vuepress/theme-default/lib/client/config.js'
+import * as clientConfig7 from 'Z:/code3/yi-doc-1/vuepress-starter/docs/.vuepress/client.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+].map((m) => m.default).filter(Boolean)
