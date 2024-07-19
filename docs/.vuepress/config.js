2ec3f36712ef9d39b2aad0689383eb74f35d4158
ZhangZiYi <ecustzzy1@163.com>
2024-07-19 22:18:22 +0800

    feat:initial commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
+  base: 'yi-doc1',
 
   title: 'VuePress',
   description: 'My first VuePress Site',
